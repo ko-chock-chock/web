0c47202ab8f3526462ee00fa579207257211c42a
yunjeongLim <630yjl@gmail.com>
2024-12-16 01:22:57 +0900

    Test: 깃허브 액션 배포 테스트

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      코촉촉 메인페이지(수정10)
+      코촉촉 메인페이지(수정11)
     </div>
   );
 }
